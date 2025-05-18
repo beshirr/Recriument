0bc35c724a8dc95e0d03a89e628732813540c108
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2025-05-18 16:29:53 +0300

    added employer home design

---
+++ EmployerHome.cs
@@ -0,0 +1,30 @@
+﻿using recruitment;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recriument
+{
+    public partial class EmployerHome : Form
+    {
+        private int employerID;
+        public EmployerHome(int employerID)
+        {
+            InitializeComponent();
+            this.employerID = employerID;
+        }
+
+        private void addVacancy_button_Click(object sender, EventArgs e)
+        {
+            VacancyForm vacancyForm = new VacancyForm();
+            vacancyForm.Show();
+            this.Hide();
+        }
+    }
+}
