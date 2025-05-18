6364a70725696d22a48327f84156702cdf479e55
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2025-05-18 14:58:12 +0300

    added update info design

---
+++ SeekerUpdateInfo.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Recriument
+{
+    partial class SeekerUpdateInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title_label = new System.Windows.Forms.Label();
+            this.experienceYears_label = new System.Windows.Forms.Label();
+            this.educationLevel_label = new System.Windows.Forms.Label();
+            this.skills_label = new System.Windows.Forms.Label();
+            this.certifications_label = new System.Windows.Forms.Label();
+            this.update_button = new System.Windows.Forms.Button();
+            this.done_button = new System.Windows.Forms.Button();
+            this.experienceYears_textbox = new System.Windows.Forms.TextBox();
+            this.skills_textbox = new System.Windows.Forms.TextBox();
+            this.certifications_textbox = new System.Windows.Forms.TextBox();
+            this.educationLevel_comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // title_label
+            // 
+            this.title_label.AutoSize = true;
+            this.title_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_label.Location = new System.Drawing.Point(269, 43);
+            this.title_label.Name = "title_label";
+            this.title_label.Size = new System.Drawing.Size(236, 29);
+            this.title_label.TabIndex = 0;
+            this.title_label.Text = "Update Seeker\'s Info";
+            // 
+            // experienceYears_label
+            // 
+            this.experienceYears_label.AutoSize = true;
+            this.experienceYears_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.experienceYears_label.Location = new System.Drawing.Point(142, 119);
+            this.experienceYears_label.Name = "experienceYears_label";
+            this.experienceYears_label.Size = new System.Drawing.Size(125, 17);
+            this.experienceYears_label.TabIndex = 1;
+            this.experienceYears_label.Text = "Experience years :";
+            // 
+            // educationLevel_label
+            // 
+            this.educationLevel_label.AutoSize = true;
+            this.educationLevel_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.educationLevel_label.Location = new System.Drawing.Point(142, 169);
+            this.educationLevel_label.Name = "educationLevel_label";
+            this.educationLevel_label.Size = new System.Drawing.Size(112, 17);
+            this.educationLevel_label.TabIndex = 2;
+            this.educationLevel_label.Text = "Education level :";
+            // 
+            // skills_label
+            // 
+            this.skills_label.AutoSize = true;
+            this.skills_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skills_label.Location = new System.Drawing.Point(142, 209);
+            this.skills_label.Name = "skills_label";
+            this.skills_label.Size = new System.Drawing.Size(48, 17);
+            this.skills_label.TabIndex = 3;
+            this.skills_label.Text = "Skills :";
+            // 
+            // certifications_label
+            // 
+            this.certifications_label.AutoSize = true;
+            this.certifications_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.certifications_label.Location = new System.Drawing.Point(142, 252);
+            this.certifications_label.Name = "certifications_label";
+            this.certifications_label.Size = new System.Drawing.Size(97, 17);
+            this.certifications_label.TabIndex = 4;
+            this.certifications_label.Text = "Certifications :";
+            // 
+            // update_button
+            // 
+            this.update_button.BackColor = System.Drawing.SystemColors.Highlight;
+            this.update_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update_button.Location = new System.Drawing.Point(323, 360);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(91, 30);
+            this.update_button.TabIndex = 5;
+            this.update_button.Text = "Update";
+            this.update_button.UseVisualStyleBackColor = false;
+            // 
+            // done_button
+            // 
+            this.done_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.done_button.Location = new System.Drawing.Point(443, 361);
+            this.done_button.Name = "done_button";
+            this.done_button.Size = new System.Drawing.Size(88, 29);
+            this.done_button.TabIndex = 6;
+            this.done_button.Text = "Done";
+            this.done_button.UseVisualStyleBackColor = true;
+            // 
+            // experienceYears_textbox
+            // 
+            this.experienceYears_textbox.Location = new System.Drawing.Point(341, 116);
+            this.experienceYears_textbox.Name = "experienceYears_textbox";
+            this.experienceYears_textbox.Size = new System.Drawing.Size(170, 20);
+            this.experienceYears_textbox.TabIndex = 7;
+            // 
+            // skills_textbox
+            // 
+            this.skills_textbox.Location = new System.Drawing.Point(341, 206);
+            this.skills_textbox.Name = "skills_textbox";
+            this.skills_textbox.Size = new System.Drawing.Size(170, 20);
+            this.skills_textbox.TabIndex = 8;
+            // 
+            // certifications_textbox
+            // 
+            this.certifications_textbox.Location = new System.Drawing.Point(341, 249);
+            this.certifications_textbox.Name = "certifications_textbox";
+            this.certifications_textbox.Size = new System.Drawing.Size(170, 20);
+            this.certifications_textbox.TabIndex = 9;
+            // 
+            // educationLevel_comboBox
+            // 
+            this.educationLevel_comboBox.FormattingEnabled = true;
+            this.educationLevel_comboBox.Location = new System.Drawing.Point(341, 165);
+            this.educationLevel_comboBox.Name = "educationLevel_comboBox";
+            this.educationLevel_comboBox.Size = new System.Drawing.Size(170, 21);
+            this.educationLevel_comboBox.TabIndex = 10;
+            // 
+            // SeekerUpdateInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.educationLevel_comboBox);
+            this.Controls.Add(this.certifications_textbox);
+            this.Controls.Add(this.skills_textbox);
+            this.Controls.Add(this.experienceYears_textbox);
+            this.Controls.Add(this.done_button);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.certifications_label);
+            this.Controls.Add(this.skills_label);
+            this.Controls.Add(this.educationLevel_label);
+            this.Controls.Add(this.experienceYears_label);
+            this.Controls.Add(this.title_label);
+            this.Name = "SeekerUpdateInfo";
+            this.Text = "SeekerUpdateInfo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title_label;
+        private System.Windows.Forms.Label experienceYears_label;
+        private System.Windows.Forms.Label educationLevel_label;
+        private System.Windows.Forms.Label skills_label;
+        private System.Windows.Forms.Label certifications_label;
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.Button done_button;
+        private System.Windows.Forms.TextBox experienceYears_textbox;
+        private System.Windows.Forms.TextBox skills_textbox;
+        private System.Windows.Forms.TextBox certifications_textbox;
+        private System.Windows.Forms.ComboBox educationLevel_comboBox;
+    }
+}
---
+++ SeekerUpdateInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recriument
+{
+    public partial class SeekerUpdateInfo : Form
+    {
+        private int currentID;
+        public SeekerUpdateInfo(int currentID)
+        {
+            InitializeComponent();
+            this.currentID = currentID;
+        }
+
+    }
+}
