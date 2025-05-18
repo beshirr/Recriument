e83a34193e17c61fe698db4cdd978456304c6a6e
beshir <youssefahmedbeshir@gmail.com>
2025-05-18 13:01:37 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Recriument
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SeekerHome());
         }
     }
 }
